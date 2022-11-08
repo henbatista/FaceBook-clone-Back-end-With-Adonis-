40e70784e99dee18c3749c75cd220c1ad9283f63
Henrique Batista <wellington.h.batista@hotmail.com>
2022-11-08 13:24:08 -0300

    Created  config for routes api

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -3,23 +3,27 @@ import Post from 'App/Models/Post'
 
 export default class PostsController {
   public async index({}: HttpContextContract) {
-    const post = await Post.all()
+    const posts = await Post.all()
 
-    return
+    return posts
   }
 
   public async store({ request }: HttpContextContract) {
     const data = request.only(['title', 'content'])
     const post = await Post.create(data)
+
+    return post
   }
 
   public async show({ params }: HttpContextContract) {
     const post = await Post.findOrFail(params.id)
+
+    return post
   }
 
   public async update({ request, params }: HttpContextContract) {
     const post = await Post.findOrFail(params.id)
-    const data = request.only(['title, content'])
+    const data = request.only(['title', 'content'])
 
     post.merge(data)
 
